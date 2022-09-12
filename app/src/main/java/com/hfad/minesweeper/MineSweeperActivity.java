87404ee3f9b5ec1a780b43316ff6cd10b124664c
Matthew Rosenthal <mar34006@usc.edu>
2022-09-12 11:57:01 -0700

    created landing page to display 8x10 grid of cells. Implemented method to create 4 random bombs and used a set to store their indices in the grid. Created countNearbyBombs method to count how mnay bombs surround a given cell.

---
+++ app/src/main/java/com/hfad/minesweeper/MineSweeperActivity.java
@@ -0,0 +1,115 @@
+package com.hfad.minesweeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
+
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+public class MineSweeperActivity extends AppCompatActivity {
+
+    private static final int ROW_COUNT = 10;
+    private static final int COLUMN_COUNT = 8;
+    private static final int NUM_BOMBS = 4;
+    private static boolean FIRST_CLICK = true;
+    Set<Pair<Integer,Integer>> bombs = new HashSet<>();
+    Set<Pair<Integer,Integer>> dug_cells = new HashSet<>();
+
+    // save the TextViews of all cells in an array, so later on,
+    // when a TextView is clicked, we know which cell it is
+    private ArrayList<TextView> cell_tvs;
+
+    private int dpToPixel(int dp) {
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        return Math.round(dp * density);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine_sweeper);
+
+        cell_tvs = new ArrayList<TextView>();
+        GridLayout grid = (GridLayout) findViewById(R.id.gridLayout01);
+        LayoutInflater li = LayoutInflater.from(this);
+
+        for (int i = 0; i < NUM_BOMBS; i++){
+            while(true) {
+                Random rand = new Random();
+                int random_row = rand.nextInt(9);
+                int random_col = rand.nextInt(7);
+                Pair<Integer, Integer> curr_bomb = new Pair<>(random_row, random_col);
+                if (!bombs.contains(curr_bomb)){
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < ROW_COUNT; i++) {
+            for (int j = 0; j < COLUMN_COUNT ; j++) {
+                TextView tv = (TextView) li.inflate(R.layout.custom_cell_layout, grid, false);
+                tv.setTextColor(Color.GREEN);
+                tv.setBackgroundColor(Color.parseColor("lime"));
+                tv.setOnClickListener(this::onClickTV);
+
+                GridLayout.LayoutParams lp = (GridLayout.LayoutParams) tv.getLayoutParams();
+                lp.rowSpec = GridLayout.spec(i);
+                lp.columnSpec = GridLayout.spec(j);
+
+                grid.addView(tv, lp);
+                cell_tvs.add(tv);
+            }
+        }
+    }
+
+    private int findIndexOfCellTextView(TextView tv) {
+        for (int n = 0; n < cell_tvs.size(); n++) {
+            if (cell_tvs.get(n) == tv)
+                return n;
+        }
+        return -1;
+    }
+
+    private int countNearbyBombs(int row, int col){
+        int count = 0;
+        for(Integer i = -1; i <= 1; i++){
+            for(Integer j = -1; j <= 1; j++) {
+                if((i >= 0 && i < ROW_COUNT) && (j >= 0 && i < COLUMN_COUNT)){
+                    Pair<Integer, Integer> curr_location = new Pair<>(i, j);
+                    if(bombs.contains(curr_location)){
+                        count += 1;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public void onClickTV(View view) {
+
+        TextView tv = (TextView) view;
+        int n = findIndexOfCellTextView(tv);
+        int row = n / COLUMN_COUNT;
+        int col = n % COLUMN_COUNT;
+        Pair<Integer, Integer> curr_location = new Pair<>(row, col);
+
+        if (tv.getCurrentTextColor() == Color.GRAY) {
+            tv.setTextColor(Color.GREEN);
+            tv.setBackgroundColor(Color.parseColor("lime"));
+        } else {
+            tv.setTextColor(Color.GRAY);
+            tv.setBackgroundColor(Color.LTGRAY);
+        }
+    }
+}
